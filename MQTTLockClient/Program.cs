28501416f405f096dc4497f3db9743fbd7b82f02
Heshoo-ksh <hishamodeh2001@gmail.com>
2023-12-15 12:43:21 -0500

    Add controller and connect it to the lcol clinet

--- MQTTLockClient/Program.cs
+++ MQTTLockClient/Program.cs
@@ -8,6 +8,8 @@ namespace MQTTLockClient
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
+            Console.WriteLine("===This is the Lock Client====.");
+
             // Initialize an MQTT client instance
             var factory = new MqttFactory();
             var mqttClient = factory.CreateMqttClient();
@@ -22,26 +24,33 @@ namespace MQTTLockClient
             {
                 //    var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
                 var message = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment.ToArray());
-                Console.WriteLine("Received message: " + message);
+                if (message == "lock")
+                {
+                    Console.WriteLine("Locking the door...");
+                    // Add further logic for 'lock' command
+                }
+                else if (message == "unlock")
+                {
+                    Console.WriteLine("Unlocking the door...");
+                    // Add further logic for 'unlock' command
+                }
                 // Add logic to handle lock/unlock commands here
             };
-
-
 
             try
             {
                 //Connect to the Broker
                 await mqttClient.ConnectAsync(options, CancellationToken.None);
                 Console.WriteLine("Connected to MQTT broker.");
-
-                //Subscribe to Command Topic (lock/commands)
-                await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("lock/commands").Build());
-                Console.WriteLine("Subscribed to 'lock/commands' topic.");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error connecting to MQTT broker: {ex.Message}");
             }
+
+            //Subscribe to Command Topic (lock/commands)
+            await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("lock/commands").Build());
+            Console.WriteLine("Subscribed to 'lock/commands' topic.");
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
---
+++ MQTTLockController/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace MQTTLockController
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("===This is the Controller Client====.");
+
+            // Initialize an MQTT client instance
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+
+            //Define Broker Connection Options: Specify the broker's address and port. For local testing, you can use localhost (127.0.0.1) and the default port 1883 (we will nbeed to change this later).
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("127.0.0.1", 1883) // Use localhost and default MQTT port
+                .Build();
+
+            try
+            {
+                //Connect to the Broker
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+                Console.WriteLine("Connected to MQTT broker.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to MQTT broker: {ex.Message}");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1: Lock");
+                Console.WriteLine("2: Unlock");
+                Console.WriteLine("3: Exit");
+                Console.Write("Enter your choice: ");
+
+                var choice = Console.ReadLine();
+
+                string payload = "";
+                switch (choice)
+                {
+                    case "1":
+                        payload = "lock";
+                        break;
+                    case "2":
+                        payload = "unlock";
+                        break;
+                    case "3":
+                        Console.WriteLine("Exiting...");
+                        if (mqttClient.IsConnected)
+                        {
+                            await mqttClient.DisconnectAsync();
+                        }
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+                        continue;
+                }
+
+                //Publish Command Functionality
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic("lock/commands")
+                    .WithPayload(payload)
+                    .Build();
+
+                await mqttClient.PublishAsync(message, CancellationToken.None);
+                Console.WriteLine($"Command '{payload}' sent.");
+            }
+        
+    }
+    }
+}
